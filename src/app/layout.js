da668b82d73252fd0afc60a2089afba11769e69c
JonasGz <jonaschagasweb@gmail.com>
2024-08-26 10:26:01 -0300

    change: font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,20 @@
 import "../styles/reset.scss";
+import { Roboto } from "next/font/google";
 
 export const metadata = {
   title: "Food Control",
   description: "Controle de gastos com lanches",
 };
 
+const roboto = Roboto({
+  weight: "400",
+  subsets: ["latin"],
+});
+
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br" suppressHydrationWarning>
-      <body>{children}</body>
+      <body className={roboto.className}>{children}</body>
     </html>
   );
 }
