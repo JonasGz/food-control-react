10703757fd20c22140e3ab7dd3d92b0e01229efb
JonasGz <jonaschagasweb@gmail.com>
2024-08-26 17:18:58 -0300

    feat: create form component

---
+++ src/app/components/Form/Form.js
@@ -0,0 +1,113 @@
+import React, { useState } from "react";
+import { useAuth } from "../../auth-config/auth-config";
+import { FaRegEye } from "react-icons/fa";
+import { FaRegEyeSlash } from "react-icons/fa";
+import "./Form.scss";
+
+const Form = ({ router, type }) => {
+  const [typePassword, setTypePassword] = useState("password");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { signIn, signUp } = useAuth();
+
+  function togglePassword() {
+    if (typePassword == "text") {
+      setTypePassword("password");
+    } else {
+      setTypePassword("text");
+    }
+  }
+
+  function onChange({ target }) {
+    if (target.type == "email") {
+      setEmail(target.value);
+    } else {
+      setPassword(target.value);
+    }
+  }
+
+  function handleAction() {
+    event.preventDefault();
+
+    if (type == "login") {
+      signIn(email, password, router);
+    } else {
+      const emailPattern =
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      const passwordPattern =
+        /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8,16}$/;
+      if (emailPattern.test(email) && passwordPattern.test(password)) {
+        signUp(email, password, router);
+      } else {
+        console.log("Email ou senha inválidos!");
+      }
+    }
+  }
+
+  return (
+    <form onSubmit={handleAction} className="container-form">
+      <div className="container-form__inputs">
+        <input
+          type="email"
+          placeholder="fulanodetal@gmail.com"
+          className="container-form__input"
+          name="email"
+          value={email}
+          onChange={onChange}
+          required
+        />
+        <div className="container-form__password">
+          <input
+            type={typePassword}
+            placeholder="**********"
+            className="container-form__input-password"
+            name="password"
+            required
+            value={password}
+            onChange={onChange}
+          />
+          <div
+            onClick={togglePassword}
+            className="container-form__eye-password"
+          >
+            {typePassword == "text" ? (
+              <FaRegEye size={18} />
+            ) : (
+              <FaRegEyeSlash size={18} />
+            )}
+          </div>
+        </div>
+      </div>
+
+      {type == "login" ? (
+        <div className="container-form__buttons">
+          <button type="submit" className="container-form__button">
+            Log in
+          </button>
+          <button
+            onClick={() => router.push("/signup")}
+            type="button"
+            className="container-form__button container-form__button--disabled"
+          >
+            Sign Up
+          </button>
+        </div>
+      ) : (
+        <div className="container-form__buttons">
+          <button type="submit" className="container-form__button">
+            Sign Up
+          </button>
+          <button
+            onClick={() => router.push("/")}
+            type="button"
+            className="container-form__button container-form__button--disabled"
+          >
+            Sign In
+          </button>
+        </div>
+      )}
+    </form>
+  );
+};
+
+export default Form;
--- src/app/page.js
+++ src/app/page.js
@@ -1,107 +1,18 @@
 "use client";
 
 import React from "react";
-import { LockOutlined, UserOutlined } from "@ant-design/icons";
-import { Button, Checkbox, Form, Input, Flex } from "antd";
 import { IoMdLock } from "react-icons/io";
 import "./page.scss";
 import { useRouter } from "next/navigation";
-import { useAuth } from "./auth-config/auth-config";
+import Form from "./components/Form/Form";
 
 export default function Home() {
-  const { signIn } = useAuth();
   const router = useRouter();
 
-  function handleSignIn(values) {
-    const email = values.username;
-    const password = values.password;
-    signIn(email, password, router);
-  }
-
   return (
-    <div className="container-login">
+    <div className="container">
       <IoMdLock className="icon-lock" size={110} />
-      <Form
-        name="login"
-        initialValues={{
-          remember: true,
-        }}
-        style={{
-          width: "100%",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-        onFinish={handleSignIn}
-      >
-        <Form.Item
-          name="username"
-          rules={[
-            {
-              required: true,
-              message: "Please input your Email!",
-            },
-          ]}
-          style={{ width: "80%" }}
-        >
-          <Input
-            style={{ height: "3rem" }}
-            prefix={<UserOutlined />}
-            placeholder="Email"
-          />
-        </Form.Item>
-        <Form.Item
-          name="password"
-          rules={[
-            {
-              required: true,
-              message: "Please input your Password!",
-            },
-          ]}
-          style={{ width: "80%" }}
-        >
-          <Input
-            prefix={<LockOutlined />}
-            type="password"
-            placeholder="Password"
-            style={{ height: "3rem" }}
-          />
-        </Form.Item>
-        <Form.Item>
-          <Flex justify="space-between" align="center">
-            <Form.Item name="remember" valuePropName="checked" noStyle>
-              <Checkbox>Remember me</Checkbox>
-            </Form.Item>
-            <a href="">Forgot password</a>
-          </Flex>
-        </Form.Item>
-
-        <Form.Item className="container-buttons">
-          <div className="buttons">
-            <Button
-              style={{ height: "3rem" }}
-              block
-              type="primary"
-              danger
-              htmlType="submit"
-            >
-              Log in
-            </Button>
-            or
-            <Button
-              style={{ height: "3rem" }}
-              block
-              type="primary"
-              onClick={() => {
-                router.push("/signup");
-              }}
-              danger
-            >
-              Sign up
-            </Button>
-          </div>
-        </Form.Item>
-      </Form>
+      <Form router={router} type="login" />
     </div>
   );
 }
